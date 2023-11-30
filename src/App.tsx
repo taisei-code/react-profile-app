9c99029f5acbaf7193345b0567f5d6da4d73f6c8
Taisei <taisei96.web@gmail.com>
2023-11-30 14:32:58 +0900

    レイアウト修正

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,32 @@ function App() {
   
   return (
     <>
-      <div className='body'>
-        <section className='profile'>
-          <h1 className='profile-title'>プロフィール自動生成</h1>
+      <div className="body">
+        <section className="profile">
+          <h1 className="profile-title">プロフィール自動生成</h1>
           <div className="profile-flex">
-            <div className="profile-item"></div>
-            <div className="profile-item"></div>
+            <div className="profile-item">
+              <div className='profile-card'>
+                <div className='profile-img'></div>
+                <div>たいせい</div>
+                <div>お誕生日:</div>
+                <div>電話番号:</div>
+              </div>
+            </div>
+            <div className="profile-item">
+              <div className="profile-input">
+                <label>お名前</label>
+                <input type="text" />
+              </div>
+              <div className="profile-input">
+                <label>お誕生日</label>
+                <input type="text" />
+              </div>
+              <div className="profile-input">
+                <label>電話番号</label>
+                <input type="text" />
+              </div>
+            </div>
           </div>
         </section>
       </div>
